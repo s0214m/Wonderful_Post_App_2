f2d270140c0267a9765dcd46bb61b25b40b1bf4a
s0214m <m.suguru.0214@gmail.com>
2022-03-13 09:14:30 +0900

    CRUD処理の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
-  # GET /articles or /articles.json
+  # GET /articles
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1 or /articles/1.json
+  # GET /articles/:id
   def show
   end
 
@@ -19,17 +19,15 @@ class ArticlesController < ApplicationController
   def edit
   end
 
-  # POST /articles or /articles.json
+  # POST /articles
   def create
     @article = Article.new(article_params)
 
     respond_to do |format|
       if @article.save
         format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,10 +37,8 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       if @article.update(article_params)
         format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,8 +48,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to root_path, notice: "Article was successfully destroyed." }
     end
   end
 
